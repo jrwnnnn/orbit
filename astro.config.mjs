bbf518f9af84d5788c7ae4c63f76b81e186d12b3
Mark Jerwin <git@jrwnnnn.me>
2026-07-30 14:49:53 +0800

    feat: Rebrand project to Orbit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-	site: "https://jrwnnnn.me",
+	site: "https://orbit.jrwnnnn.me",
 	output: "server",
 	adapter: cloudflare({
 		remoteBindings: !process.env.CI,
