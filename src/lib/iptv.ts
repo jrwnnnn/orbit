d7e3eab1002ef03bc7a7f4d2e6cda3775a3859ad
Mark Jerwin <git@jrwnnnn.me>
2026-07-30 17:46:53 +0800

    fix: Error 1102 (worker exceeding resource limits) (#4)
    * fix: Error 1102 (worker exceeding resource limits)
    
    * fix: Eslint no-unused-vars error

--- src/lib/iptv.ts
+++ src/lib/iptv.ts
@@ -1,3 +1,5 @@
+import { env } from "cloudflare:workers";
+
 export interface Channel {
 	id: string;
 	name: string;
@@ -18,7 +20,11 @@ interface Logo {
 	url: string;
 }
 
-export async function getChannelData() {
+export interface Data extends Channel {
+	logo: string | null;
+}
+
+async function fetchIPTV() {
 	const [channels, logos] = await Promise.all([
 		fetch("https://iptv-org.github.io/api/channels.json").then(
 			(res) => res.json() as Promise<Channel[]>,
@@ -29,9 +35,48 @@ export async function getChannelData() {
 	]);
 
 	const logoMap = new Map(logos.map((l) => [l.channel, l.url]));
-
-	return channels.map((channel) => ({
+	const data: Data[] = channels.map((channel) => ({
 		...channel,
 		logo: logoMap.get(channel.id) ?? null,
 	}));
+
+	return data;
+}
+
+async function refreshCache(): Promise<Data[]> {
+	const list = new Set(
+		(await env.DB.prepare("SELECT id FROM channels").all()).results.map(
+			(r) => r.id as string,
+		),
+	);
+
+	const data = (await fetchIPTV()).filter((c) => list.has(c.id));
+
+	await env.SESSION.put("cache", JSON.stringify(data), {
+		expirationTtl: 43200,
+	});
+
+	await Promise.all(
+		data.map((c) =>
+			env.SESSION.put(`channel:${c.id}`, JSON.stringify(c), {
+				expirationTtl: 43200,
+			}),
+		),
+	);
+
+	return data;
+}
+
+export async function getData(id: string): Promise<Data[]> {
+	if (id === "all") {
+		const cache = await env.SESSION.get<Data[]>("cache", "json");
+		if (cache) return cache;
+		return refreshCache();
+	} else {
+		const cached = await env.SESSION.get<Data>(`channel:${id}`, "json");
+		if (cached) return [cached];
+
+		const result = (await refreshCache()).find((c) => c.id === id);
+		return result ? [result] : [];
+	}
 }
